210411cb97bab576f04ef781adc52c0215d9d765
Mint <skylove.exe@gmail.com>
2021-03-17 18:47:06 +0900

    Initial Commit

---
+++ quickReminder/quickReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  quickReminderApp.swift
+//  quickReminder
+//
+//  Created by hato on 2021/03/17.
+//
+
+import SwiftUI
+
+@main
+struct quickReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
